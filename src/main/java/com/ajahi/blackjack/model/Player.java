ad3ca68a2700093dec49854197e1a3811ddaff26
Hama Keli <saykin@gmail.com>
2020-05-13 19:32:36 +0200

    Redid Player.java to only represent the player name, ID and registration date.

--- src/main/java/com/ajahi/blackjack/model/Player.java
+++ src/main/java/com/ajahi/blackjack/model/Player.java
@@ -1,4 +1,44 @@
 package com.ajahi.blackjack.model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 public class Player {
+    private String playerName;
+    private UUID playerId;
+    private LocalDate playerRegistrationDate;
+
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+        this.playerId = UUID.randomUUID();
+        this.playerRegistrationDate = LocalDate.now();
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public UUID getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(UUID playerId) {
+        this.playerId = playerId;
+    }
+
+    public LocalDate getPlayerRegistrationDate() {
+        return playerRegistrationDate;
+    }
+
+    public void setPlayerRegistrationDate(LocalDate playerRegistrationDate) {
+        this.playerRegistrationDate = playerRegistrationDate;
+    }
 }
