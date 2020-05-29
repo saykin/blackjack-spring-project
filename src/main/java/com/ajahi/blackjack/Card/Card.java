a1d52af669a23cd5192d152701b5258fa18efd58
Hama Keli <saykin@gmail.com>
2020-05-29 13:54:40 +0200

    Restructured and remade the Card class.

---
+++ src/main/java/com/ajahi/blackjack/Card/Card.java
@@ -0,0 +1,29 @@
+package com.ajahi.blackjack.Card;
+
+public class Card {
+    private String suit;
+    private int rank;
+
+    public Card() {}
+
+    public Card(String suit, int rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
