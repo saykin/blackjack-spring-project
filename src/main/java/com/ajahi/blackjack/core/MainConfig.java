cc423fab34f4467fb094289859bfb7576c8a729c
Hama Keli <saykin@gmail.com>
2020-05-29 14:04:06 +0200

    Added a MainConfig class, at the moment it's commented out as I'm still in the testing phase while I'm still learning Spring.

---
+++ src/main/java/com/ajahi/blackjack/core/MainConfig.java
@@ -0,0 +1,31 @@
+package com.ajahi.blackjack.core;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class MainConfig {
+
+    /*
+    @Bean
+     BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+    */
+}
+
