3936aae374d92f886f93dc89499b354d03d5937b
Hama Keli <saykin@gmail.com>
2020-05-30 18:28:08 +0200

    Added a controller class for User

---
+++ src/main/java/com/ajahi/blackjack/user/UserController.java
@@ -0,0 +1,47 @@
+package com.ajahi.blackjack.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/test-create-user")
+    public String testCreateUser() {
+        userService.addUser(new com.ajahi.blackjack.user.User("Hama", "12345678"));
+        return "User created";
+    }
+
+    @GetMapping("/test-create-multiple-users")
+    public String testCreateMultipleUsers() {
+        userService.addMultipleUsers(Arrays.asList(new User("Olav", "12345678"),
+                new User("Preben", "12345678"),
+                new User("Robin", "12345678"),
+                new User("Morgan", "12345678")));
+        return "Several players added";
+    }
+
+    @GetMapping("/print-users")
+    public String printUsers() {
+        userService.findAllUsers().forEach(user -> System.out.println(user.getUserId()));
+        return "Printing all users to log";
+    }
+
+    @GetMapping("/get-hama-username")
+    public String printSpecificUser() {
+        System.out.println(userService.findUser("Hama").getUsername());
+        return "Printing Hama to log";
+    }
+
+    @GetMapping("/get-hama-id")
+    public String printSpecificUserId() {
+        System.out.println(userService.findUserById(UUID.fromString("419a0d37-cc70-4fc4-8550-7290c70eeb00")).getUserId());
+        return "Printing ID of Hama to log";
+    }
+}
