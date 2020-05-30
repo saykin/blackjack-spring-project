f14f6f5d85e0f3ff1ca40833ba92ed3e465d3dde
Hama Keli <saykin@gmail.com>
2020-05-30 18:20:41 +0200

    Removed imports in MainConfig class until this goes online on my heroku app

--- src/main/java/com/ajahi/blackjack/core/MainConfig.java
+++ src/main/java/com/ajahi/blackjack/core/MainConfig.java
@@ -1,13 +1,5 @@
 package com.ajahi.blackjack.core;
 
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
-@Configuration
 public class MainConfig {
 
     /*
