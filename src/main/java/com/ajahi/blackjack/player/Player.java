888ee7c9cbe37b7a2aafdfdbb49f76de78a180db
Hama Keli <saykin@gmail.com>
2020-05-30 18:26:19 +0200

    Split Player some player variables to User to better keep track of persistent values and non persistent ones.

--- src/main/java/com/ajahi/blackjack/player/Player.java
+++ src/main/java/com/ajahi/blackjack/player/Player.java
@@ -1,118 +1,42 @@
 package com.ajahi.blackjack.player;
 
 import com.ajahi.blackjack.Card.Card;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import com.ajahi.blackjack.user.User;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
-@Entity
 public class Player {
-
-    @Id
-    @GeneratedValue(generator = "UUID")
-    public static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder(10);
-    private UUID playerId;
-
-    @JsonIgnore
-    @Size(min = 8)
-    private String password;
-
-    @NotNull
-    @Size(min = 3, max = 30, message = "Player name must be between 3 and 30 letters")
-    private String playerName;
-
-    @NotNull
-    private int playerRank;
-
-    @NotNull
+    private final User user;
+    private final String username;
+    private final List<Card> cardsDrawn;
+    private int cardValue;
+    private boolean hasAce;
     private int playerBank;
 
-    @NotNull
-    private LocalDate playerRegistrationDate;
-
-    private List<Card> cardsDrawn;
-
-    private int cardValue;
-
-    private boolean hasAce;
-
-    @NotNull
-    private String role;
-
-    public Player() {
+    public Player(User user) {
+        this.user = user;
+        this.username = user.getUsername();
+        this.cardsDrawn = new ArrayList<>();
+        this.cardValue = 0;
+        this.hasAce = false;
+        this.playerBank = user.getUserBank();
     }
 
-    public Player(String playerName, String password) {
-        this.playerName = playerName;
-        setPassword(password);
-        playerBank = 2000;
-        playerId = UUID.randomUUID();
-        playerRegistrationDate = LocalDate.now();
-        cardsDrawn = new ArrayList<>();
+    public User getUser() {
+        return user;
     }
 
-    public UUID getPlayerId() {
-        return playerId;
-    }
-
-    public void setPlayerId(UUID playerId) {
-        this.playerId = playerId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = PASSWORD_ENCODER.encode(password);
-    }
-
-    public String getPlayerName() {
-        return playerName;
-    }
-
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
-    }
-
-    public int getPlayerRank() {
-        return playerRank;
-    }
-
-    public void setPlayerRank(int playerRank) {
-        this.playerRank = playerRank;
-    }
-
-    public int getPlayerBank() {
-        return playerBank;
-    }
-
-    public void setPlayerBank(int playerBank) {
-        this.playerBank = playerBank;
-    }
-
-    public LocalDate getPlayerRegistrationDate() {
-        return playerRegistrationDate;
-    }
-
-    public void setPlayerRegistrationDate(LocalDate playerRegistrationDate) {
-        this.playerRegistrationDate = playerRegistrationDate;
+    public String getUsername() {
+        return username;
     }
 
     public List<Card> getCardsDrawn() {
         return cardsDrawn;
     }
 
-    public void addDrawnCard(Card card) {
-        cardsDrawn.add(card);
+    public boolean addDrawnCard(Card card) {
+        return cardsDrawn.add(card);
     }
 
     public int getCardValue() {
@@ -120,7 +44,7 @@ public class Player {
     }
 
     public void setCardValue(int cardValue) {
-        this.cardValue += cardValue;
+        this.cardValue = cardValue;
     }
 
     public boolean isHasAce() {
@@ -131,11 +55,11 @@ public class Player {
         this.hasAce = hasAce;
     }
 
-    public String getRole() {
-        return role;
+    public int getPlayerBank() {
+        return playerBank;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    public void setPlayerBank(int playerBank) {
+        this.playerBank = playerBank;
     }
 }
