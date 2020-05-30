579a3a2dd60bbd497cb101430d23e7a218e6a4ff
Hama Keli <saykin@gmail.com>
2020-05-30 18:28:35 +0200

    Added a service layer for User class

---
+++ src/main/java/com/ajahi/blackjack/user/UserService.java
@@ -0,0 +1,33 @@
+package com.ajahi.blackjack.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void addMultipleUsers(List<User> users) {
+        userRepository.saveAll(users);
+    }
+
+    public User findUser(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User findUserById(UUID uuid) {
+        return userRepository.findById(uuid).orElseThrow(NullPointerException::new);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+}
