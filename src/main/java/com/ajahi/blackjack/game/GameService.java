c89c2883a53f1cd8387b7afcaf2bed8e2784b950
Hama Keli <saykin@gmail.com>
2020-05-30 18:24:59 +0200

    Added a addPlayerToGameSession method.

--- src/main/java/com/ajahi/blackjack/game/GameService.java
+++ src/main/java/com/ajahi/blackjack/game/GameService.java
@@ -1,4 +1,21 @@
 package com.ajahi.blackjack.game;
 
+import com.ajahi.blackjack.player.PlayerService;
+import com.ajahi.blackjack.user.User;
+import com.ajahi.blackjack.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class GameService {
+    private final UserService userService;
+    private final PlayerService playerService;
+
+    @Autowired
+    public GameService(UserService userService, PlayerService playerService) {
+        this.userService = userService;
+        this.playerService = playerService;
+    }
+
+    public void addPlayerToGameSession(User user) {
+        playerService.addPlayer(user);
+    }
 }
