924fcae50ce5d23b31ecb8e4ac5b903e1feda07a
Hama Keli <saykin@gmail.com>
2020-05-31 16:51:02 +0200

    Added some methods to GameService class

--- src/main/java/com/ajahi/blackjack/game/GameService.java
+++ src/main/java/com/ajahi/blackjack/game/GameService.java
@@ -1,21 +1,50 @@
 package com.ajahi.blackjack.game;
 
+import com.ajahi.blackjack.player.Player;
 import com.ajahi.blackjack.player.PlayerService;
 import com.ajahi.blackjack.user.User;
 import com.ajahi.blackjack.user.UserService;
+import com.devskiller.friendly_id.FriendlyId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
 public class GameService {
     private final UserService userService;
     private final PlayerService playerService;
+    private final Map<UUID, Game> gameSessions;
 
     @Autowired
     public GameService(UserService userService, PlayerService playerService) {
         this.userService = userService;
         this.playerService = playerService;
+        gameSessions = new HashMap<>();
     }
 
-    public void addPlayerToGameSession(User user) {
-        playerService.addPlayer(user);
+    public String createGameSession() {
+        UUID sessionId = UUID.randomUUID();
+        System.out.println(sessionId);
+        Game game = new Game(sessionId);
+        gameSessions.put(sessionId, game);
+        return game.getGameUrl();
+    }
+
+    public void addPlayerToGameSession(User user, String sessionUrl) {
+        System.out.println(sessionUrl);
+        UUID sessionId = FriendlyId.toUuid(sessionUrl);
+        System.out.println(sessionId);
+        gameSessions.get(sessionId).addPlayerToSession(playerService.addPlayer(user));
+    }
+
+    public boolean findGameSession(String sessionUrl) {
+        return (gameSessions.get(FriendlyId.toUuid(sessionUrl)) != null);
+    }
+
+    public void deleteSession(String sessionUrl) {
+        gameSessions.remove(FriendlyId.toUuid(sessionUrl));
     }
 }
