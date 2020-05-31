14afbdb9157ca958d209d2a516ab95b71a1dc961
Hama Keli <saykin@gmail.com>
2020-05-31 16:50:28 +0200

    Added addPlayer method

--- src/main/java/com/ajahi/blackjack/player/PlayerService.java
+++ src/main/java/com/ajahi/blackjack/player/PlayerService.java
@@ -2,25 +2,27 @@ package com.ajahi.blackjack.player;
 
 import com.ajahi.blackjack.card.CardService;
 import com.ajahi.blackjack.user.User;
+import com.ajahi.blackjack.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlayerService {
     private final CardService cardService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public PlayerService(CardService cardService) {
+    public PlayerService(CardService cardService, UserRepository userRepository) {
         this.cardService = cardService;
+        this.userRepository = userRepository;
     }
-
-
 
     public void addCard() {
 
     }
 
-    public void addPlayer(User user) {
-
+    public Player addPlayer(User user) {
+        Player newPlayer = new Player(user);
+        return newPlayer;
     }
 }
