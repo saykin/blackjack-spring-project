2feea19c5cf10cf002ff884f1d8d112d789b84c3
Hama Keli <saykin@gmail.com>
2020-06-03 21:44:21 +0200

    Changed from UUID key in Map to String that is based on session URL.

--- src/main/java/com/ajahi/blackjack/game/GameService.java
+++ src/main/java/com/ajahi/blackjack/game/GameService.java
@@ -4,19 +4,17 @@ import com.ajahi.blackjack.player.Player;
 import com.ajahi.blackjack.player.PlayerService;
 import com.ajahi.blackjack.user.User;
 import com.ajahi.blackjack.user.UserService;
-import com.devskiller.friendly_id.FriendlyId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 @Service
 public class GameService {
     private final UserService userService;
     private final PlayerService playerService;
-    private final Map<UUID, Game> gameSessions;
+    private final Map<String, Game> gameSessions;
 
     @Autowired
     public GameService(UserService userService, PlayerService playerService) {
@@ -26,24 +24,29 @@ public class GameService {
     }
 
     public String createGameSession() {
-        UUID sessionId = UUID.randomUUID();
-        System.out.println(sessionId);
-        Game game = new Game(sessionId);
-        gameSessions.put(sessionId, game);
+        Game game = new Game();
+        gameSessions.put(game.getGameUrl(), game);
+        Player dealer = createNewDealerForSession();
+        gameSessions.get(game.getGameUrl()).addPlayerToSession(dealer);
         return game.getGameUrl();
     }
 
+    private Player createNewDealerForSession() {
+        User user = new User();
+        user.setUsername("House");
+        Player player = new Player(user);
+        return player;
+    }
+
     public void addPlayerToGameSession(String username, String sessionUrl) {
-        UUID sessionId = FriendlyId.toUuid(sessionUrl);
-        User user = userService.findUser(username);
-        gameSessions.get(sessionId).addPlayerToSession(playerService.addPlayer(user));
+        gameSessions.get(sessionUrl).addPlayerToSession(playerService.createPlayer(userService.findUser(username)));
     }
 
     public boolean findGameSession(String sessionUrl) {
-        return (gameSessions.get(FriendlyId.toUuid(sessionUrl)) != null);
+        return gameSessions.get(sessionUrl) != null;
     }
 
     public void deleteSession(String sessionUrl) {
-        gameSessions.remove(FriendlyId.toUuid(sessionUrl));
+        gameSessions.remove(sessionUrl);
     }
 }
